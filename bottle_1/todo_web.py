53de4b604a59a0592f7db606fec9330bab276d6b
JulinaM <julinamaharjan@gmail.com>
2020-09-14 20:18:19 -0400

    sept 14

--- bottle_1/todo_web.py
+++ bottle_1/todo_web.py
@@ -1,14 +1,33 @@
-from bottle import route, run, template
+from bottle import get, post, run, template, request, debug,redirect
 import sqlite3
 
 
-@route('/todos')
-def get_todos():
+@get('/')
+def get_show_list():
     connection = sqlite3.connect("todo.db")
     cursor = connection.cursor()
     cursor.execute("select * from todo")
     result = cursor.fetchall()
-    return str(result)
+    cursor.close()
+    return template("show_list", rows=result)
 
 
-run(host='localhost', port=8080)
+@get("/new_item")
+def get_new_item():
+    return template("new_item")
+
+
+@post("/new_item")
+def post_new_item():
+    new_item = request.forms.get("new_item").strip()
+    connection = sqlite3.connect("todo.db")
+    connection.execute("INSERT INTO todo(task, status) VALUES (?, ?)", (new_item, 1))
+    connection.commit()
+    connection.close()
+    # return "The new item is [" + str(new_item) + "]..."
+    redirect("/")
+
+debug(True) #comment out
+run(host='localhost', port=8080) #comment out
+
+# application = default_app() # to host
